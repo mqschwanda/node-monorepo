e6be6be6e4b221ddcc6fac54a005c4149f42eb46
mqschwanda <mqschwanda@gmail.com>
2018-08-30 14:23:27 -0400

    upgrade rollup-config-default

--- packages/scripts/rollup.config.js
+++ packages/scripts/rollup.config.js
@@ -1,61 +1,16 @@
 import {
-  mergeDefaultNpmConfig,
-  // mergeDefaultNodeConfig,
+  mergeDefaultNodeConfig,
   mergeDefaultExecutableConfig,
+  getExternals,
+  replacePlugins,
 } from '@mqschwanda/rollup-config-default';
-
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
 
-import packageJSON, { name, bin } from './package.json';
+import packageJSON, { name } from './package.json';
 
-const getExternals = ({
-  dependencies = {},
-  devDependencies = {},
-  peerDependencies = {},
-}) => [
-  ...Object.keys(dependencies),
-  ...Object.keys(devDependencies),
-  ...Object.keys(peerDependencies),
-];
-
-const isDefined = n => n !== undefined;
-const isDuplicate = (plugin, i, self) =>
-  i === self.findIndex(({ name }) => name === plugin.name);
-
-const replacePlugins = (oldConfig, plugins = []) => {
-  const config = {
-    ...oldConfig,
-    plugins: oldConfig.plugins.filter(isDefined).filter(isDuplicate),
-  };
-
-  plugins.forEach(plugin => {
-    const i = config.plugins.findIndex(({ name }) => name === plugin.name);
-
-    if (i > -1) config.plugins[i] = plugin;
-    else config.plugins.push(plugin);
-  });
-
-  return config;
-}
-
-const trimPlugins = (oldConfig, plugins = []) => {
-  const config = {
-    ...oldConfig,
-    plugins: oldConfig.plugins.filter(isDefined).filter(isDuplicate),
-  };
-
-  plugins.forEach(plugin => {
-    const i = plugins.findIndex(({ name }) => name === plugin.name);
-    if (i > -1) config.plugins.splice(i, 1);
-  });
-
-  return config;
-}
-
-const nodeConfig = replacePlugins(mergeDefaultNpmConfig({
-// const nodeConfig = replacePlugins(mergeDefaultNodeConfig({
+const nodeConfig = replacePlugins(mergeDefaultNodeConfig({
   input: 'src/index.js',
   output: {
     name,
@@ -74,7 +29,6 @@ const nodeConfig = replacePlugins(mergeDefaultNpmConfig({
     exclude: '**/node_modules/**',
     plugins: ['external-helpers'],
   }),
-  commonjs(),
   uglify(),
 ]);
 
@@ -85,6 +39,15 @@ const executableConfig = replacePlugins(mergeDefaultExecutableConfig({
     file: 'dist/handle-unhandled-rejections.js',
   },
 }), [
+  babel({
+    babelrc: false,
+    presets: [
+      ['env', { modules: false }],
+      'stage-3',
+    ],
+    exclude: '**/node_modules/**',
+    plugins: ['external-helpers'],
+  }),
   uglify(),
 ]);
 
