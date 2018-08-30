2b0141d85e5445763695e1e5e475d92d3d163ab4
mqschwanda <mqschwanda@gmail.com>
2018-08-30 16:32:52 -0400

    remove trimming in rollup.config

--- packages/scripts/rollup.config.js
+++ packages/scripts/rollup.config.js
@@ -2,7 +2,6 @@ import {
   mergeDefaultNodeConfig,
   mergeDefaultExecutableConfig,
   getExternals,
-  replacePlugins,
 } from '@mqschwanda/rollup-config-default';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
@@ -10,7 +9,7 @@ import { uglify } from 'rollup-plugin-uglify';
 
 import packageJSON, { name } from './package.json';
 
-const nodeConfig = replacePlugins(mergeDefaultNodeConfig({
+const nodeConfig = mergeDefaultNodeConfig({
   input: 'src/index.js',
   output: {
     name,
@@ -19,37 +18,39 @@ const nodeConfig = replacePlugins(mergeDefaultNodeConfig({
     'child_process',
     ...getExternals(packageJSON)
   ],
-}), [
-  babel({
-    babelrc: false,
-    presets: [
-      ['env', { modules: false }],
-      'stage-3',
-    ],
-    exclude: '**/node_modules/**',
-    plugins: ['external-helpers'],
-  }),
-  uglify(),
-]);
+  plugins: [
+    babel({
+      babelrc: false,
+      presets: [
+        ['env', { modules: false }],
+        'stage-3',
+      ],
+      exclude: '**/node_modules/**',
+      plugins: ['external-helpers'],
+    }),
+    uglify(),
+  ],
+});
 
-const executableConfig = replacePlugins(mergeDefaultExecutableConfig({
+const executableConfig = mergeDefaultExecutableConfig({
   input: 'src/handle-unhandled-rejections.js',
   output: {
     name: 'handle-unhandled-rejections',
     file: 'dist/handle-unhandled-rejections.js',
   },
-}), [
-  babel({
-    babelrc: false,
-    presets: [
-      ['env', { modules: false }],
-      'stage-3',
-    ],
-    exclude: '**/node_modules/**',
-    plugins: ['external-helpers'],
-  }),
-  uglify(),
-]);
+  plugins: [
+    babel({
+      babelrc: false,
+      presets: [
+        ['env', { modules: false }],
+        'stage-3',
+      ],
+      exclude: '**/node_modules/**',
+      plugins: ['external-helpers'],
+    }),
+    uglify(),
+  ],
+});
 
 export default [
   nodeConfig,
