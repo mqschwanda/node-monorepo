dccd025077a9b23b4e280e3e4b8fa1f396750ffd
mqschwanda <mqschwanda@gmail.com>
2018-08-30 13:21:20 -0400

    add some more config objects

--- packages/rollup-config-default/rollup.config.js
+++ packages/rollup-config-default/rollup.config.js
@@ -1,31 +1,52 @@
 import babel from 'rollup-plugin-babel';
+import executable from 'rollup-plugin-executable';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
+
+/**
+ * this config file is being imported directly into the src and bundled into
+ * into the module. We need to make sure that requiring the `package.json` will
+ * not throw an error so we wrap it in a try catch block.
+ */
+let packageJSON;
+try {
+  packageJSON = require('./package.json');
+} catch (e) {
+  packageJSON = {
+    name: '',
+    dependencies: [],
+  };
+}
+
+const { name, dependencies } = packageJSON;
 
 export default {
   input: 'src/index.js',
   plugins: [
     babel({
-      plugins: ['external-helpers'],
+      babelrc: false,
+      presets: [
+        ['env', { modules: false }],
+        'stage-3',
+      ],
+      exclude: '**/node_modules/**',
+      plugins: [
+        'external-helpers',
+      ],
     }),
     uglify(),
+    nodeResolve({
+      preferBuiltins: false,
+    }),
+    commonjs(),
   ],
   output: {
     file: 'dist/index.js',
     exports: 'named',
-    name: '@mqschwanda/rollup-config-default',
-    format: 'umd',
+    name,
+    format: 'cjs',
     sourceMap: true,
-    globals: {
-      'deepmerge': 'deepmerge',
-      'rollup-plugin-babel': 'babel',
-      'rollup-plugin-executable': 'executable',
-      'rollup-plugin-uglify': 'uglify',
-    }
   },
-  external: [ // <-- suppresses the warning
-    'deepmerge',
-    'rollup-plugin-babel',
-    'rollup-plugin-executable',
-    'rollup-plugin-uglify',
-  ],
+  external: dependencies ? Object.keys(dependencies) : undefined,
 }
--- packages/rollup-config-default/src/index.js
+++ packages/rollup-config-default/src/index.js
@@ -1,54 +1,123 @@
-import deepmerge from 'deepmerge';
+// import compose from '@mqschwanda/compose';
+import merge from 'deepmerge';
 import executable from 'rollup-plugin-executable';
-import _config from '../rollup.config';
+import nodeResolve from 'rollup-plugin-node-resolve';
+// import globals from 'rollup-plugin-node-globals';
+// import builtins from 'rollup-plugin-node-builtins';
+import commonjs from 'rollup-plugin-commonjs';
+import config from '../rollup.config';
+
+/**
+ * remove any plugins that are being added from the new configuration object.
+ * This will prevent duplication errors
+ * @param  {[type]} oldConfig [description]
+ * @param  {[type]} newConfig [description]
+ * @return {[type]}           [description]
+ */
+export const trimPlugins = (oldConfig, newConfig) => {
+  const config = oldConfig;
+
+  (newConfig.plugins || []).forEach(plugin => {
+    const i = newConfig.plugins.findIndex(({ name }) => name === plugin.name);
+    if (i > -1) delete config.plugins[i];
+  });
+
+  return config;
+}
 
 /**
  * remove package specific config so a more generalized config can be gernerated
  * @param  {[type]} config [description]
  * @return {[type]}        [description]
  */
-const trimConfig = (c) => {
-  if (c.output) {
-    if (c.output.name) delete c.output.name;
-    if (c.output.globals) delete c.output.globals;
+export const trimConfig = (oldConfig, newConfig) => {
+  const config = oldConfig;
+  if (config.output) {
+    if (config.output.name) delete config.output.name;
+    if (config.output.globals) delete config.output.globals;
   }
 
-  if (c.externals) delete c.externals;
+  if (config.external) delete config.external;
 
-  return c;
+  return config;
 };
 
-const mergeConfig = (oldConfig, newConfig) =>
-  deepmerge(trimConfig(oldConfig), newConfig);
+// export const trim = compose(trimConfig, trimPlugins);
+
+export const replacePlugins = (oldConfig, plugins = []) => {
+  const config = oldConfig;
+
+  plugins.forEach(plugin => {
+    const i = config.plugins.findIndex(({ name }) => name === plugin.name);
+
+    if (i > -1) config.plugins[i] = plugin;
+    else config.plugins.push(plugin);
+  });
+
+  return config;
+};
+
+export const mergeConfig = (oldConfig, newConfig) =>
+  // merge(trim(oldConfig, newConfig), newConfig);
+  merge(trimPlugins(trimConfig(oldConfig), newConfig), newConfig);
 
 /**
  * default package configuration
  * @type {[type]}
  */
-export const config = _config;
-export const defaultConfig = _config;
+export { config };
+export const defaultConfig = config;
 
 export const mergeDefaultConfig = (newConfig) =>
   mergeConfig(defaultConfig, newConfig);
 
 /**
- * npm package configuration
+ * npm package configuration. This is for code that is bundled to be published
+ * to npm. This will optimize the code for both browser and node where possible.
  * @type {[type]}
  */
-const defaultNpmConfig = defaultConfig;
+export const defaultNpmConfig = mergeDefaultConfig({});
 
 export const mergeDefaultNpmConfig = (newConfig) =>
   mergeConfig(defaultNpmConfig, newConfig);
 
 /**
- * npm executable package configuration
+ * node package configuration. This is for code that is bundled to run on node
+ * only (not optimized for browser).
  * @type {[type]}
  */
-const defaultExecutablConfig = mergeDefaultNpmConfig({
-  plugins: [executable()],
+export const defaultNodeConfig = mergeDefaultNpmConfig({
+  output: {
+    format: 'cjs',
+  },
+  plugins: [
+    nodeResolve({
+      preferBuiltins: true,
+    }),
+    commonjs(),
+    // globals(),
+    // builtins(),
+  ]
+});
+
+export const mergeDefaultNodeConfig = (newConfig) =>
+  mergeConfig(defaultNodeConfig, newConfig);
+
+/**
+ * executable package configuration. This is for code that is bundled to run as
+ * a cli or shell script.
+ * @type {[type]}
+ */
+export const defaultExecutableConfig = mergeDefaultNodeConfig({
+  output: {
+    format: 'cjs',
+  },
+  plugins: [
+    executable(),
+  ],
 });
 
 export const mergeDefaultExecutableConfig = (newConfig) =>
-  mergeConfig(defaultExecutablConfig, newConfig);
+  mergeConfig(defaultExecutableConfig, newConfig);
 
 export default config;
